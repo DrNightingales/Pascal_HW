fc6bc5f35937bf0eb9c7339881bfa18acc23505b
Peter Soloviev <petersoloviev123@gmail.com>
2022-11-06 19:34:54 +0300

    10.17j completed

--- Homework_3/10.17j/10.17j.pas
+++ Homework_3/10.17j/10.17j.pas
@@ -1,5 +1,54 @@
 program t1017j;
-
+const 
+	n = 50;
+	m = 40;
+var 
+	words: array[1..n] of string[m];
+	s: string[m];
+	i, j: integer;
+	c, w: char;
+	new_word: boolean;
+	seen: array['A'..'z'] of boolean;
 begin
-     writeln('hwllo');
+	for c := 'A' to 'z' do
+		seen[c] := false;
+	new_word := false;
+	i := 1;
+	s := '';
+	read(c);
+	while c <> '.' do
+	begin
+		if c = ' ' then
+		begin
+			if new_word then
+			begin
+				words[i] := s;
+				s := '';
+				i := i + 1;
+				for w := 'A' to 'z' do
+					seen[w] := false;
+				new_word := false;
+			end;
+			read(c);
+			continue;
+		end;
+		new_word := true;
+		if seen[c] then
+		begin
+			s := '';
+			repeat
+				read(c);
+			until (c = ' ') or (c = '.');
+		end
+		else
+		begin
+			s := s + c;
+			seen[c] := true;
+			read(c);
+		end;
+	end;
+	words[i] := s;
+	for j := 1 to i-1 do
+		if words[j] <> words[i] then
+			writeln(words[j]);
 end.
