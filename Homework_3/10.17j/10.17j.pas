d4e7b435a993efe4c3b2796257ef31d7a841f27f
DrNightingales <petersoloviev123@gmail.com>
2022-11-09 02:45:26 +0300

    Source code reformatted by ptop

--- Homework_3/10.17j/10.17j.pas
+++ Homework_3/10.17j/10.17j.pas
@@ -1,54 +1,57 @@
-program t1017j;
-const 
-	n = 50;
-	m = 40;
-var 
-	words: array[1..n] of string[m];
-	s: string[m];
-	i, j: integer;
-	c, w: char;
-	new_word: boolean;
-	seen: array['A'..'z'] of boolean;
-begin
-	for c := 'A' to 'z' do
-		seen[c] := false;
-	new_word := false;
-	i := 1;
-	s := '';
-	read(c);
-	while c <> '.' do
-	begin
-		if c = ' ' then
-		begin
-			if new_word then
-			begin
-				words[i] := s;
-				s := '';
-				i := i + 1;
-				for w := 'A' to 'z' do
-					seen[w] := false;
-				new_word := false;
-			end;
-			read(c);
-			continue;
-		end;
-		new_word := true;
-		if seen[c] then
-		begin
-			s := '';
-			repeat
-				read(c);
-			until (c = ' ') or (c = '.');
-		end
-		else
-		begin
-			s := s + c;
-			seen[c] := true;
-			read(c);
-		end;
-	end;
-	words[i] := s;
-	for j := 1 to i-1 do
-		if words[j] <> words[i] then
-			writeln(words[j]);
-end.
+
+Program t1017j;
+
+Const 
+    n =   50;
+    m =   40;
+
+Var 
+    words:   array[1..n] Of string[m];
+    s:   string[m];
+    i, j:   integer;
+    c, w:   char;
+    new_word:   boolean;
+    seen:   array['A'..'z'] Of boolean;
+Begin
+    For c := 'A' To 'z' Do
+        seen[c] := false;
+    new_word := false;
+    i := 1;
+    s := '';
+    read(c);
+    While c <> '.' Do
+        Begin
+            If c = ' ' Then
+                Begin
+                    If new_word Then
+                        Begin
+                            words[i] := s;
+                            s := '';
+                            i := i + 1;
+                            For w := 'A' To 'z' Do
+                                seen[w] := false;
+                            new_word := false;
+                        End;
+                    read(c);
+                    continue;
+                End;
+            new_word := true;
+            If seen[c] Then
+                Begin
+                    s := '';
+                    Repeat
+                        read(c);
+                    Until (c = ' ') Or (c = '.');
+                End
+            Else
+                Begin
+                    s := s + c;
+                    seen[c] := true;
+                    read(c);
+                End;
+        End;
+    words[i] := s;
+    For j := 1 To i-1 Do
+        If words[j] <> words[i] Then
+            writeln(words[j]);
+End.
--- Homework_3/10.18g/10.18g.pas
+++ Homework_3/10.18g/10.18g.pas
@@ -1,45 +1,48 @@
-program t1018g;
-const 
-	n = 50;
-	m = 40;
-var 
-	words: array[1..n] of string[m];
-	s: string[m];
-	i, j, l: integer;
-	c: char;
-	new_word: boolean;
-begin
-	new_word := false;
-	i := 1;
-	s := '';
-	read(c);
-	while c <> '.' do
-	begin
-		if c = ' ' then
-		begin
-			if new_word then
-			begin
-				words[i] := s;
-				s := '';
-				i := i + 1;
-				new_word := false;
-			end;
-			read(c);
-			continue;
-		end;
-		new_word := true;
-		s := s + c;
-		read(c);
-	end;
-	words[i] := s;
-	for j := 1 to i-1 do
-	begin
-		l := Length(words[j]);
-		if words[j] <> words[i] then
-		begin
-			if l > 1 then
-				delete(words[j], l, l);
-			writeln(words[j]);
-		end;
-	end;
-end.
+
+Program t1018g;
+
+Const 
+    n =   50;
+    m =   40;
+
+Var 
+    words:   array[1..n] Of string[m];
+    s:   string[m];
+    i, j, l:   integer;
+    c:   char;
+    new_word:   boolean;
+Begin
+    new_word := false;
+    i := 1;
+    s := '';
+    read(c);
+    While c <> '.' Do
+        Begin
+            If c = ' ' Then
+                Begin
+                    If new_word Then
+                        Begin
+                            words[i] := s;
+                            s := '';
+                            i := i + 1;
+                            new_word := false;
+                        End;
+                    read(c);
+                    continue;
+                End;
+            new_word := true;
+            s := s + c;
+            read(c);
+        End;
+    words[i] := s;
+    For j := 1 To i-1 Do
+        Begin
+            l := Length(words[j]);
+            If words[j] <> words[i] Then
+                Begin
+                    If l > 1 Then
+                        delete(words[j], l, l);
+                    writeln(words[j]);
+                End;
+        End;
+End.
