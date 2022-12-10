24c1373932a510a81712eaee64e654f58f45aa8a
DrNightingales <petersoloviev123@gmail.com>
2022-12-10 23:17:58 +0300

    Added some tasks

---
+++ Homework_4/11.56/11.56.pas
@@ -0,0 +1,27 @@
+
+Program t1156;
+
+
+Function Sum_of_divisors(K: integer):   integer;
+
+Var sum, i:   integer;
+Begin
+    sum := 1;
+    For i := 2 To (k-1) Do
+        If k Mod i = 0 Then
+            sum := sum + i;
+    Sum_of_divisors := sum;
+End;
+
+Var 
+    sums:   array[2..maxint] Of integer;
+    N, i, j:   integer;
+Begin
+    read(N);
+    For i := 2 To N Do
+        sums[i] := Sum_of_divisors(i);
+    For i := 2 To N Do
+        For j := i+1 To N Do
+            If (sums[i] = j) And (sums[j] = i) Then
+                write(i, ' ', j, ' ');
+End.
